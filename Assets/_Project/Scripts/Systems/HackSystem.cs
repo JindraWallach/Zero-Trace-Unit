98989d311d625f239f8496b5307aa52149be4ac8
JindraWallach <149482038+JindraWallach@users.noreply.github.com>
2025-10-12 19:26:52 +0200

    Pre-Hack

---
+++ Assets/_Project/Scripts/Systems/HackSystem.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class HackSystem : MonoBehaviour
+{
+    public enum HackState
+    {
+        Inactive,
+        Hacking,
+        Success,
+        Failure,
+        Interrupted
+    }
+
+
+}
